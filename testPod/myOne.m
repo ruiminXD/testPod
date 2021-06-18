bbafd098cde1d3cecbf1b3eb6f334a120c3a6a19
jessy <zhangruimin@xd.com>
2021-06-18 16:29:59 +0800

    test

---
+++ testPod/myOne.m
@@ -0,0 +1,18 @@
+//
+//  myOne.m
+//  testPod
+//
+//  Created by jessy on 2021/6/18.
+//
+
+#import "myOne.h"
+
+@implementation myOne
+
++ (NSString *)test{
+    NSString *str = @"库里面的测试数据";
+    NSLog(@"%@",str);
+    return str;
+}
+
+@end
